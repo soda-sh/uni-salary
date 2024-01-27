f914fc4128b6e8185cf69c10b54cb871d1b36ed6
Maziar-M <maziar.mir.2002@gmail.com>
2024-01-28 00:52:01 +0330

    Update classes.py
    implemented Prof.add()

--- classes.py
+++ classes.py
@@ -1,25 +1,41 @@
 # This file contains all the classes for the app
 
+# database connection is created in main app file, we just use the cursor here
+# commiting the changes to the database (e.g. after add method) happens in main app file
+import mysql.connector
+
 class Prof():
     # profDetails is a dict() where keys are names of the columns in the table
     def __init__(self, profDetails):
         self.firstName = profDetails['firstName']
         self.lastName = profDetails['lastName']
         self.id = profDetails['id']
+        self.nationalId = profDetails['nationalId']
         self.base = profDetails['base']
         self.grade = profDetails['grade']
 
+
     # adds the prof to the database
-    def add(self):
-        # to be added
+    def add(self, dbCursor):
+        sqlStatement = (
+            "INSERT INTO profs"
+            "(firstName, lastName, nationalId, base, grade)"
+            f"VALUES ({self.firstName}, {self.lastName}, {self.nationalId}, {self.base}, {self.grade})"
+            )
+
+        # maybe add error handling later
+        dbCursor.execute(sqlStatement)
+
 
     # updates currently existing prof in the database
-    def edit(self):
+    def edit(self, dbCursor):
         # to be added
 
+
     # deletes prof from the database
-    def delete(self):
+    def delete(self, dbCursor):
         # to be added
+
 
     # calculates prof's salary
     def calculateSalary(self):
@@ -40,14 +56,17 @@ class Thesis():
         self.status = thesisDetails['status']
         self.grade = thesisDetails['grade']
 
+
     # addes thesis to the database
-    def add(self):
+    def add(self, dbCursor):
         # to be added
+
 
     # edits currently existing thesis in the database
-    def edit(self):
+    def edit(self, dbCursor):
         # to be added
 
+
     # deletes thesis from the database
-    def delete(self):
+    def delete(self, dbCursor):
         # to be added
