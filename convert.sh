ac59be035740318d0da6dbc58decde06063f7a93
hossein <hosteam01@gmail.com>
2024-01-29 00:11:52 +0330

    rename for bash tab completion

--- convert.sh
+++ convert.sh
@@ -6,7 +6,7 @@ cd raw-ui/
 for i in *.ui; do
 	j=$(echo ${i} | sed 's/ui$/py/')
 	echo ${i} ${j}
-		pyuic5 -x "${i}" -o "../${j}"
+		pyuic5 -x "${i}" -o "../tmp-${j}"
 done
 
 pyrcc5 -o ../logo_rc.py ../logo.qrc
