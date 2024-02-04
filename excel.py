6a338597b9dcdfaddde1a01ca8d94291ed5ca1d8
Maziar-M <maziar.mir.2002@gmail.com>
2024-02-04 23:06:04 +0330

    Add excel wraper

---
+++ excel.py
@@ -0,0 +1,20 @@
+import openpyxl as excel
+import openpyxl.utils as utils
+
+
+class Excel:
+
+    # searches for a value inside a column, then returns the first matching row
+    def search(workbook, col, value):
+        wb = excel.load_workbook(workbook)
+        ws = wb.active
+
+        colLetter = utils.get_column_letter(col)
+        for item in ws[colLetter]:
+            if item.value == value:
+                row = item.row
+
+        for row in ws.iter_rows(min_row=row, max_row=row, values_only=True):
+            result = row
+
+        return result
